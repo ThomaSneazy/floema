8ee0d99a92c933689a099b993ca269faf3f670df
thomascarre <carrethomas7@gmail.com>
2023-11-07 23:37:37 +0100

    add prismic API but node 18 cant run with prismicDom

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
-const express = require("express");
+require('dotenv').config()
+
+const express = require('express');
 const app = express();
 const path = require('path')
 const port = 3000;
+
+const Prismic = require('@prismicio/client')
+const PrismicDOM = require('@prismic-dom');
+
+const initApi = req => {
+  return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req
+  })
+}
+
+const handleLinkResolver = doc => {
+  if (doc.type === 'page'){
+    return '/page' + doc.uid;
+  } else if (doc.type === 'blog_post'){
+    return '/blog' + doc.uid;
+  }
+  return '/'
+}
+
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: handleLinkResolver,
+  };
+  res.locals.PrismicDOM = PrismicDOM
+
+  next()
+})
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
@@ -10,9 +41,15 @@ app.get("/", (req, res) => {
   res.render('pages/home');
 });
 
-app.get("/about", (req, res) => {
-  res.render('pages/about');
+app.get("/about", async (req, res) => {
+  initApi(req).then((api) => {
+    api.query(Prismic.Predicates.at('document.type', 'about')).then((response) => {
+      console.log(response);
+      res.render('pages/about');
+    });
+  });
 });
+
 
 app.get("/collections", (req, res) => {
   res.render('pages/collections');
