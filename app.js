8214bf4ff5ba5560c23010f2bd049476c5684866
thomascarre <carrethomas7@gmail.com>
2023-11-09 16:48:35 +0100

    add prismic APIIIIIIIIIIII finally

--- app.js
+++ app.js
@@ -6,21 +6,36 @@ const path = require('path')
 const port = 3000;
 
 const Prismic = require('@prismicio/client')
-const PrismicDOM = require('@prismic-dom');
+const PrismicDOM = require('prismic-dom');
 
-const initApi = req => {
-  return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
-    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
-    req
-  })
-}
+// const initApi = req => {
+//   return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
+//     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+//     req
+//   })
+// }
+const initApi = async (req) => {
+  try {
+    const api = await Prismic.api(process.env.PRISMIC_ENDPOINT, {
+      accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+      req,
+    });
+    return api;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+
+
 
 const handleLinkResolver = doc => {
-  if (doc.type === 'page'){
-    return '/page' + doc.uid;
-  } else if (doc.type === 'blog_post'){
-    return '/blog' + doc.uid;
-  }
+  // if (doc.type === 'page'){
+  //   return '/page' + doc.uid;
+  // } else if (doc.type === 'blog_post'){
+  //   return '/blog' + doc.uid;
+  // }
   return '/'
 }
 
@@ -37,19 +52,26 @@ app.use((req, res, next) => {
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
+app.get('/about', async (req, res) => {
+  initApi(req).then(api => {
+    api.query(Prismic.Predicates.any('document.type', ['about', 'meta'])).then(response => {
+      const { results } = response
+      const [meta, about] = results
+
+      // console.log(meta, about);
+
+      res.render('pages/about', {
+        about,
+        meta
+      })
+    })
+  })
+})
+
+
 app.get("/", (req, res) => {
   res.render('pages/home');
 });
-
-app.get("/about", async (req, res) => {
-  initApi(req).then((api) => {
-    api.query(Prismic.Predicates.at('document.type', 'about')).then((response) => {
-      console.log(response);
-      res.render('pages/about');
-    });
-  });
-});
-
 
 app.get("/collections", (req, res) => {
   res.render('pages/collections');
